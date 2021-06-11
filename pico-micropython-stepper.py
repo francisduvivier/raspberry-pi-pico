c1651610c5ddcb7e3f80dde73e33a0c67e43f96b
Francis Duvivier <francisduvivier@gmail.com>
2021-06-11 19:48:31 +0200

    Add more comments regarding running and documentation

--- pico-micropython-stepper.py
+++ pico-micropython-stepper.py
@@ -1,4 +1,7 @@
-# To use this file, upload it to your raspberry pi pico using the Thonny IDE.
+# To use this file, upload it to your raspberry pi pico 
+# with the file name main.py
+# using the Thonny IDE.
+# For more info, check https://projects.raspberrypi.org/en/projects/getting-started-with-the-pico/9
 from machine import Pin
 import time
 pins = [Pin(14, Pin.OUT), Pin(15,Pin.OUT), Pin(16,Pin.OUT), Pin(17,Pin.OUT)]
